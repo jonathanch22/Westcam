63199d713f340454dd9de156e714ecb0a047135d
JonathanChui <jonathanalex0230@gmail.com>
2026-06-24 09:42:07 -0400

    Update supabaseCurso.js

--- supabaseCurso.js
+++ supabaseCurso.js
@@ -1,7 +1,7 @@
-import { createClient } from 'https://cdn.skypack.dev/@supabase/supabase-js';
+// Usamos el mismo CDN de jsdelivr que tus otros archivos
+import { createClient } from 'https://cdn.jsdelivr.net/npm/@supabase/supabase-js/+esm';
 
 const supabaseUrl = 'https://kfgfriaanvvujgxxksbj.supabase.co'; 
 const supabaseAnonKey = 'sb_publishable_6ZJ_VzxXeU3BgLHIJYYbow_a7fCiGeS'; 
 
-// Lo exportamos con un nombre único para este módulo
 export const clienteCurso = createClient(supabaseUrl, supabaseAnonKey);
