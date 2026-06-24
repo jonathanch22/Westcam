2c72dfdc341efe07cb0fd0accdd6d9f4f919f60f
JonathanChui <jonathanalex0230@gmail.com>
2026-06-23 20:36:00 -0400

    Delete supabaseClient.js

--- supabaseClient.js
+++
@@ -1,7 +0,0 @@
-// supabaseClient.js
-import { createClient } from 'https://esm.sh/@supabase/supabase-js@2';
-
-const supabaseUrl = 'https://kfgfriaanvvujgxxksbj.supabase.co/rest/v1/';
-const supabaseKey = 'sb_publishable_6ZJ_VzxXeU3BgLHIJYYbow_a7fCiGeS';
-
-export const supabase = createClient(supabaseUrl, supabaseKey);
