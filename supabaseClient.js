073d73e9378e9bdfbb2d0139b6a39577943afc4e
JonathanChui <jonathanalex0230@gmail.com>
2026-06-23 21:39:57 -0400

    Delete supabaseClient.js

--- supabaseClient.js
+++
@@ -1,9 +0,0 @@
-import { createClient } from 'https://cdn.skypack.dev/@supabase/supabase-js';
-
-// Revisa que esta línea tenga el formato correcto y las comillas simples
-const supabaseUrl = 'https://kfgfriaanvvujgxxksbj.supabase.co'; 
-
-// Esta es la clave que ya copiaste bien de tu imagen
-const supabaseAnonKey = 'sb_publishable_6ZJ_VzxXeU3BgLHIJYYbow_a7fCiGeS'; 
-
-export const supabase = createClient(supabaseUrl, supabaseAnonKey);
