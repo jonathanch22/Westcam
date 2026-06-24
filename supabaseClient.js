787da5ad8d409608020c71c72f4de98f097eac5a
JonathanChui <jonathanalex0230@gmail.com>
2026-06-23 23:31:31 -0400

    Update supabaseClient.js

--- supabaseClient.js
+++ supabaseClient.js
@@ -62,3 +62,11 @@ if (error) {
     console.log("¡ÉXITO! Datos guardados:", data);
     alert("¡Guardado correctamente!");
 }
+
+// supabaseClient.js
+import { createClient } from 'https://cdn.skypack.dev/@supabase/supabase-js';
+
+const supabaseUrl = 'https://kfgfriaanvvujgxxksbj.supabase.co'; 
+const supabaseAnonKey = 'sb_publishable_6ZJ_VzxXeU3BgLHIJYYbow_a7fCiGeS'; 
+
+export const supabase = createClient(supabaseUrl, supabaseAnonKey);
